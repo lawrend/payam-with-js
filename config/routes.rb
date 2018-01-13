cac82d91f99b3adbbc6b31407a9f980def9ce8d3
lawrend <douglas_lawrence@mac.com>
2018-01-13 10:53:13 -0800

    added routes from old payam app but did used payams instead of corpses

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,22 @@
 Rails.application.routes.draw do
-  devise_for :users
+
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources lines, :except => [:update, :edit]
+  resources :payams do 
+      member do 
+          post 'decompose'
+      end 
+  end 
+ resources :players do 
+     resources :payams 
+     resources :lines
+ end 
+  root 'welcome#index'
+  get 'welcome/about'
+
+
+  # keep user id out of the visible url
+  get 'player' => 'players#show'
+
 end
