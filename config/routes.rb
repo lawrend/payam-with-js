af705b2ea8927923148cf6f20a22c7aa80895c40
lawrend <douglas_lawrence@mac.com>
2018-01-13 11:29:19 -0800

    added styles to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,27 @@
 Rails.application.routes.draw do
+    devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources lines, :except => [:update, :edit]
-  resources :payams do 
-      member do 
-          post 'decompose'
-      end 
-  end 
- resources :players do 
-     resources :payams 
-     resources :lines
- end 
-  root 'welcome#index'
-  get 'welcome/about'
+    resources lines, :except => [:update, :edit]
+    
+    resources :payams do
+        member do
+            post 'decompose'
+        end 
+    end 
+    
+    resources :players do
+        resources :payams 
+        resources :lines
+    end 
+ 
+    resources :styles do 
+        resources :payams 
+    end 
 
+    root 'welcome#index'
+    get 'welcome/about'
 
-  # keep user id out of the visible url
-  get 'player' => 'players#show'
+    # keep user id out of the visible url
+    get 'player' => 'players#show'
 
 end
