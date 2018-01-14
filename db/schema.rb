12f8ed61d727afa0bf93a1a8f3f9702750714d56
lawrend <douglas_lawrence@mac.com>
2018-01-14 03:02:14 -0800

    added username to user for login and fixed bootstrap.css.sass name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180113194855) do
+ActiveRecord::Schema.define(version: 20180114110128) do
 
   create_table "lines", force: :cascade do |t|
     t.text     "text"
@@ -47,8 +47,10 @@ ActiveRecord::Schema.define(version: 20180113194855) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
