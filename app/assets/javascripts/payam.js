5d2037748b609e62ef11de95af4a888cb933ded1
lawrend <douglas_lawrence@mac.com>
2018-02-09 20:11:12 -0800

    fixed preview button

--- app/assets/javascripts/payam.js
+++ app/assets/javascripts/payam.js
@@ -3,6 +3,8 @@
 let previewIt = function(preview_button) {
     let id = $(preview_button).data("id");
     let prev_box = $('#preview-payam-' + id);
+
+    if (prev_box.text() == "") {
     let stuf = $.ajax({
         url: "/payams/" + id + "/lines/",
         type: "get",
@@ -10,12 +12,12 @@ let previewIt = function(preview_button) {
             payam_id: id
         },
     });
-
     stuf.done(function(resp) {
         let stuf_resp = resp['data'];
         stuf_resp.forEach(function(el) {
             prev_box.append("<p>Line " + el['attributes']['count'] + ": " + el['attributes']['text'] + "</p>");
         });
-    });
+      });
+    };
 };
 
