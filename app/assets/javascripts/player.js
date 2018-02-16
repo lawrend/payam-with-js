bf70cff9f680ca9b2d5dd6716dbb20decc3b412c
lawrend <douglas_lawrence@mac.com>
2018-02-16 06:15:35 -0800

    forward and back next payam buttons functional

--- app/assets/javascripts/player.js
+++ app/assets/javascripts/player.js
@@ -1,6 +1,9 @@
 let firstPayam = function(playerId) {
     let position = parseInt($('#iron_maiden').data('id'));
+    // let nextPosition = parseInt(position + 1);
     let nextPosition = parseInt($('#iron_maiden').data('id')) + 1;
+    // let prevPosition = parseInt(position - 1);
+    let prevPosition = parseInt($('#iron_maiden').data('id')) - 1;
     $.get("/players/"+ playerId +"/payams.json").done(function(resp) {
     let tuba = resp['data'];
     let tabble = tuba[position]['attributes']['title']
@@ -9,9 +12,40 @@ let firstPayam = function(playerId) {
             nextPosition = 0;
         };
 
-    $('#nexties').html("<p class='markee'>Title: <a href='/payams/"+tuba[position]['id']+"'>"+tabble+"</a></p><button class='btn btn-default' id='iron_maiden' data-id='"+nextPosition+"' onclick='firstPayam("+playerId+")'>Next one...</button>")
+        if(prevPosition < 0) {
+            prevPosition = longo - 1;
+        };
+    console.log(prevPosition);
+
+    $('#nexties').html("<p class='markee'>Title: <a href='/payams/"+tuba[position]['id']+"'>"+tabble+"</a></p><button class='btn btn-default' id='scorps' data-id='"+prevPosition+"' onclick='prevPayam("+playerId+")'>Previous</button><button class='btn btn-default' id='iron_maiden' data-id='"+nextPosition+"' onclick='firstPayam("+playerId+")'>Next one...</button>")
     });
 };
+
+let prevPayam = function(playerId) {
+    let position = parseInt($('#scorps').data('id'));
+    // let nextPosition = position + 1;
+
+    let nextPosition = parseInt($('#scorps').data('id')) + 1;
+    // let prevPosition = position - 1;
+    let prevPosition = parseInt($('#scorps').data('id')) - 1;
+    $.get("/players/"+ playerId +"/payams.json").done(function(resp) {
+    let tuba = resp['data'];
+    let tabble = tuba[position]['attributes']['title'];
+    let longo = tuba.length;
+        if(nextPosition >= longo) {
+            nextPosition = 0;
+        };
+
+        if(prevPosition < 0) {
+            prevPosition = longo - 1;
+        };
+
+    console.log(prevPosition);
+
+    $('#nexties').html("<p class='markee'>Title: <a href='/payams/"+tuba[position]['id']+"'>"+tabble+"</a></p><button class='btn btn-default' id='scorps' data-id='"+prevPosition+"' onclick='prevPayam("+playerId+")'>Previous</button><button class='btn btn-default' id='iron_maiden' data-id='"+nextPosition+"' onclick='firstPayam("+playerId+")'>Next one...</button>")
+    });
+};
+
 
 
 $(function() {
