97c50ca42cd3dfb9ec69693e134ac710701a4ab3
lawrend <douglas_lawrence@mac.com>
2018-02-16 23:40:55 -0800

    changed font colors and fixed much indentation

--- app/assets/javascripts/player.js
+++ app/assets/javascripts/player.js
@@ -1,13 +1,16 @@
+// First time button is pressed and every time "Next" pressed
 let firstPayam = function(playerId) {
+    //get position in array of finished payams, value stored in button
     let position = parseInt($('#iron_maiden').data('id'));
-    // let nextPosition = parseInt(position + 1);
     let nextPosition = parseInt($('#iron_maiden').data('id')) + 1;
-    // let prevPosition = parseInt(position - 1);
     let prevPosition = parseInt($('#iron_maiden').data('id')) - 1;
+
+    //GET all completed payams for this player
     $.get("/players/"+ playerId +"/payams.json").done(function(resp) {
-    let tuba = resp['data'];
-    let tabble = tuba[position]['attributes']['title']
-    let longo = tuba.length
+        let tuba = resp['data'];
+        let tabble = tuba[position]['attributes']['title']
+        let longo = tuba.length
+        //allow for looping through array
         if(nextPosition >= longo) {
             nextPosition = 0;
         };
@@ -15,12 +18,12 @@ let firstPayam = function(playerId) {
         if(prevPosition < 0) {
             prevPosition = longo - 1;
         };
-    console.log(prevPosition);
-
-    $('#nexties').html("<p class='markee'>Title: <a href='/payams/"+tuba[position]['id']+"'>"+tabble+"</a></p><button class='btn btn-default' id='scorps' data-id='"+prevPosition+"' onclick='prevPayam("+playerId+")'>Previous</button><button class='btn btn-default' id='iron_maiden' data-id='"+nextPosition+"' onclick='firstPayam("+playerId+")'>Next one...</button>")
+        //replace html with new buttons with new values
+        $('#nexties').html("<p class='markee'>Title: <a href='/payams/"+tuba[position]['id']+"'>"+tabble+"</a></p><button class='btn btn-default' id='scorps' data-id='"+prevPosition+"' onclick='prevPayam("+playerId+")'>Previous</button><button class='btn btn-default' id='iron_maiden' data-id='"+nextPosition+"' onclick='firstPayam("+playerId+")'>Next one...</button>")
     });
 };
 
+//each time "previous" button is pressed
 let prevPayam = function(playerId) {
     let position = parseInt($('#scorps').data('id'));
     // let nextPosition = position + 1;
@@ -29,9 +32,9 @@ let prevPayam = function(playerId) {
     // let prevPosition = position - 1;
     let prevPosition = parseInt($('#scorps').data('id')) - 1;
     $.get("/players/"+ playerId +"/payams.json").done(function(resp) {
-    let tuba = resp['data'];
-    let tabble = tuba[position]['attributes']['title'];
-    let longo = tuba.length;
+        let tuba = resp['data'];
+        let tabble = tuba[position]['attributes']['title'];
+        let longo = tuba.length;
         if(nextPosition >= longo) {
             nextPosition = 0;
         };
@@ -40,9 +43,8 @@ let prevPayam = function(playerId) {
             prevPosition = longo - 1;
         };
 
-    console.log(prevPosition);
-
-    $('#nexties').html("<p class='markee'>Title: <a href='/payams/"+tuba[position]['id']+"'>"+tabble+"</a></p><button class='btn btn-default' id='scorps' data-id='"+prevPosition+"' onclick='prevPayam("+playerId+")'>Previous</button><button class='btn btn-default' id='iron_maiden' data-id='"+nextPosition+"' onclick='firstPayam("+playerId+")'>Next one...</button>")
+        //replace html with new buttons with new values
+        $('#nexties').html("<p class='markee'>Title: <a href='/payams/"+tuba[position]['id']+"'>"+tabble+"</a></p><button class='btn btn-default' id='scorps' data-id='"+prevPosition+"' onclick='prevPayam("+playerId+")'>Previous</button><button class='btn btn-default' id='iron_maiden' data-id='"+nextPosition+"' onclick='firstPayam("+playerId+")'>Next one...</button>")
     });
 };
 
